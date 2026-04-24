3f8504eaed0bc670539b2d8ecec30e5d4a634404
Ayberk Aktan <ayberk.akt.991@gmail.com>
2026-04-24 15:31:10 +0300

    Create a Simulink models and motor PID control bugs fixed.

--- Code/BTS7960_Driver_PID_Control_Code/BTS7960_Driver_PID_Control_Code.ino
+++ Code/BTS7960_Driver_PID_Control_Code/BTS7960_Driver_PID_Control_Code.ino
@@ -5,35 +5,38 @@
 #define LPWM 11 
 #define RPWM 10 
 
-volatile int posi = 0;
+volatile long posi = 0;
 float ePrev = 0;
+unsigned long prevT = 0;
 float eIntegral = 0; 
-long prevT = 0;
+float lastDTerm = 0;
 
 void setup() {
-  pinMode(LEN,OUTPUT);
-  pinMode(REN,OUTPUT);
-  pinMode(LPWM,OUTPUT);
-  pinMode(RPWM,OUTPUT);
-
-  pinMode(ENCA,INPUT_PULLUP);
-  pinMode(ENCB,INPUT_PULLUP);
-
+  pinMode(LEN, OUTPUT); 
+  pinMode(REN, OUTPUT);
+  pinMode(LPWM, OUTPUT); 
+  pinMode(RPWM, OUTPUT);
+  pinMode(ENCA, INPUT_PULLUP);
+  pinMode(ENCB, INPUT_PULLUP);
   digitalWrite(LEN, HIGH);
   digitalWrite(REN, HIGH);
-
-  attachInterrupt(digitalPinToInterrupt(ENCA),countMotor,RISING);
+  attachInterrupt(digitalPinToInterrupt(ENCA), countMotor, RISING);
 }
 
 void loop() {
-  float Kp = 5;
-  float Ki = 0.05;
-  float Kd = 0.05;
+  //PID Factors from Simulink Models
+  const double Kp = 2.9735;
+  const double Ki = 0.3001;
+  const double Kd = 6.5378;
+  const double N = 181.6;
+  
+  int target = 24;
 
-  float target = 48;
-
-  long currT = micros();
-  float deltaT = ((float)(currT - prevT)/1e6);
+  unsigned long currT = micros();
+  float deltaT = ((currT - prevT) / 1e6);
+  if(deltaT < 0.01){
+    return;
+  }
   prevT = currT;
 
   int pos = 0;
@@ -41,18 +44,28 @@ void loop() {
   pos = posi;
   interrupts();
 
-  float error = target - pos;
+  int error = target - pos;
 
-  eIntegral += error * deltaT;
+  eIntegral += error * deltaT; 
+  
+  //Integral Windup
+  if(eIntegral > 1){
+    eIntegral = 1;
+  }else if(eIntegral < -1){
+    eIntegral = -1;
+  }
 
-  float ed = ((error - ePrev)/deltaT);
+  float eD = ((error - ePrev)/deltaT);
+  float dTerm = lastDTerm + (N * deltaT * (eD - lastDTerm));
+  lastDTerm = dTerm;
 
-  float u = (Kp * error) + (Ki * eIntegral) + (Kd * ed);
+  float u = (Kp * error) + (Ki * eIntegral) + (Kd * dTerm);
 
-  float pwr = fabs(u);
-
-  if(pwr > 255){
-    pwr = 255;
+  int pwr = abs(u); 
+  
+  // 255 => 24V , 127 => 12V for motors nominal voltage
+  if(pwr > 127){
+    pwr = 127;
   }
 
   int dir = 1;
@@ -61,35 +74,34 @@ void loop() {
     dir = -1;
   }
 
-  if(fabs(error) < 5){
+  if(abs(error) < 2){
     eIntegral = 0;
     setMotor(0,0);
   }else{
-    setMotor(dir,(int)pwr);
+    setMotor(dir,pwr);
   }
 
   ePrev = error;
 }
 
-void setMotor(int dir , int ppwr){
-  if(dir == 1){
-    digitalWrite(LPWM,LOW);
-    analogWrite(RPWM,ppwr);
-  }else if(dir == -1){
-    digitalWrite(RPWM,LOW);
-    analogWrite(LPWM,ppwr);
-  }else{
-    digitalWrite(RPWM,LOW);
-    digitalWrite(LPWM,LOW);
+void setMotor(int dir, int ppwr) {
+  if (dir == 1) {
+    digitalWrite(LPWM, LOW);
+    analogWrite(RPWM, ppwr);
+  } else if (dir == -1) {
+    digitalWrite(RPWM, LOW);
+    analogWrite(LPWM, ppwr);
+  } else {
+    digitalWrite(LPWM, HIGH);
+    digitalWrite(RPWM, HIGH);
   }
 }
 
-void countMotor(){
-  int b = digitalRead(ENCB);
-
-  if(b > 0){
+void countMotor() {
+  if (digitalRead(ENCB) > 0){
     posi++;
-  }else{
+  }
+  else{ 
     posi--;
   }
 }
